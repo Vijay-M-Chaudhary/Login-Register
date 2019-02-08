8f19c7df8751367d8739ac357710b6d51100bb46
Vijay Chaudhary <34163436+Vijay-M-Chaudhary@users.noreply.github.com>
2019-02-08 16:18:40 +0530

    Login form V1

---
+++ src/app/_models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    cnfpassword: string;
+    email: string;
+    mobile: string;
+    address: string;
+}
